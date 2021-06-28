cc8b6a2182316b9fcf0b52ad27a9ae9272d58a50
saurav <saurav.saurav.dhungana@gmail.com>
2021-06-28 21:22:04 +0545

    basic ui added

--- script.js
+++ script.js
@@ -17,60 +17,70 @@ function computerPlay(){
 
 
 function game(){
+    let rockUrl= "https://image.shutterstock.com/image-vector/hand-that-rocks-finger-linear-600w-1916914304.jpg" ;
+    let paperUrl = "https://image.shutterstock.com/image-vector/business-man-hand-hold-paper-600w-373891489.jpg" ;
+    let scissorUrl = "https://image.shutterstock.com/image-photo/female-hand-holds-hairdresser-scissors-600w-1780121387.jpg" ;
     let playerScore = 0;
     let computerScore =0;
-    for(let i = 0; i<=4; i++){
-    const playerSelection = prompt("Enter rock paper or scissor:").toLowerCase();
+   
+    window.addEventListener('click', (e) =>{
+    const playerSelection = e.target.alt;
     const computerSelection = computerPlay();
-        function playRound(playerSelection,computerSelection){
-            if(playerSelection === 'rock' && computerSelection === 'paper'){
-                console.log(i,"You lose! Paper beats rock.");
-                computerScore++;
-            }
-            else if(playerSelection === 'rock' && computerSelection === 'scissor'){
-                console.log(i,"Congrats! Rock beats Scissor");
-                playerScore++;
-            } 
-            else if (playerSelection==='rock' && computerSelection === 'rock'){
-                console.log(i,"Its a draw !");
-            }
-            else if(playerSelection==='paper' && computerSelection==='rock') {
-                console.log(i,"Congrat's Paper beats rock.");
-                playerScore++;
-            }
-            else if(playerSelection==='paper' && computerSelection ==='paper'){
-                console.log(i,"Its a draw !");
-            }
-            else if(playerSelection==='paper' && computerSelection ==='scissor'){
-                console.log(i,"You lose! Scissor beats paper");
-                computerScore++;
-            }
-            else if(playerSelection==='scissor' && computerSelection ==='rock'){
-                console.log(i,"You lose! rock beats scissor");
-                computerScore++;
-            }
-            else if(playerSelection==='scissor' && computerSelection ==='paper'){
-                console.log(i,"Congrats! scissor beats rock");
-                playerScore++;
-            }
-            else if(playerSelection==='scissor' && computerSelection ==='scissor'){
-                console.log(i,"Its a draw!")
-            }
+    const computerSelectionDisplay = document.querySelector('.machine-side');
+    const resultDisplay = document.querySelector('.result-display');
+    const winResult =  '<h1 style="color:green;padding:0">Congrats you win</h1>';
+    const loseResult =  '<h1 style="color:red;padding:0">Too bad you lose.</h1>';
+    const drawResult =  '<h1 style="color:black;padding:0">Its a draw.</h1>';
+    function playRound(playerSelection,computerSelection){
+        if(playerSelection === 'rock' && computerSelection === 'paper'){
+            computerSelectionDisplay.innerHTML = `<img src= ${paperUrl} class="computer-selection">`;
+            resultDisplay.innerHTML = loseResult;
+        }
+        else if(playerSelection === 'rock' && computerSelection === 'scissor'){
+            computerSelectionDisplay.innerHTML = `<img src= ${scissorUrl}>`;
+            resultDisplay.innerHTML = winResult;
+        } 
+        else if (playerSelection==='rock' && computerSelection === 'rock'){
+            computerSelectionDisplay.innerHTML = `<img src= ${rockUrl}>`;
+            resultDisplay.innerHTML = drawResult;
+        }
+        else if(playerSelection==='paper' && computerSelection==='rock') {
+            computerSelectionDisplay.innerHTML = `<img src= ${rockUrl}>`;
+            resultDisplay.innerHTML = winResult;
+        }
+        else if(playerSelection==='paper' && computerSelection ==='paper'){
+            computerSelectionDisplay.innerHTML = `<img src= ${paperUrl}>`;
+            resultDisplay.innerHTML = drawResult;
+        }
+        else if(playerSelection==='paper' && computerSelection ==='scissor'){
+            computerSelectionDisplay.innerHTML = `<img src= ${scissorUrl}>`;
+            resultDisplay.innerHTML = loseResult;
             
-            }
-            playRound(playerSelection,computerSelection);
-            console.log(playerSelection,computerSelection);
-    } 
+        }
+        else if(playerSelection==='scissor' && computerSelection ==='rock'){
+            computerSelectionDisplay.innerHTML = `<img src= ${rockUrl}>`;
+            resultDisplay.innerHTML = loseResult;
+        }
+        else if(playerSelection==='scissor' && computerSelection ==='paper'){
+            computerSelectionDisplay.innerHTML = `<img src= ${paperUrl}>`;
+            resultDisplay.innerHTML = winResult;
+        }
+        else if(playerSelection==='scissor' && computerSelection ==='scissor'){
+            computerSelectionDisplay.innerHTML = `<img src= ${scissorUrl}>`;
+            resultDisplay.innerHTML = drawResult;
+        }
+        
+        }
+        playRound(playerSelection,computerSelection);
+        console.log(playerSelection,computerSelection);
+    }); 
     
-    if(playerScore>computerScore){
-        console.log("you win the final game!")
-    }
-    else if(playerScore===computerScore){
-        console.log("The final game is a draw.")
-    }
-    else{
-        console.log("You lose the final game!")
-    }
+
+    
+ 
+    
+    
+
 }
 
 game();
